6cabc59700dade104d9121b2aa4edb8caef716ba
Sas Florin <sasflorindarius@gmail.com>
2024-03-07 20:45:02 +0200

    aggregations

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -164,6 +164,45 @@ const deleteTour = async (req, res) => {
   }
 };
 
+const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'easy' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 module.exports = {
   getAllTours,
   getTour,
@@ -171,4 +210,5 @@ module.exports = {
   deleteTour,
   createTour,
   aliasTopTours,
+  getTourStats,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,7 @@ const {
   deleteTour,
   createTour,
   aliasTopTours,
+  getTourStats,
 } = require('../controllers/tourController');
 
 const router = express.Router();
@@ -13,6 +14,8 @@ const router = express.Router();
 // router.param('id', checkID);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route(`/:id`).get(getTour).patch(updateTour).delete(deleteTour);
